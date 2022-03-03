ee95c8020394de68ecbc4952da3555034ab52aa1
Vinay Kumar Verma <vkverma0210@gmail.com>
2022-03-03 22:53:24 +0530

    WavePrint

---
+++ DSA/WavePrint2DArray.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int col = 0; col < n; col++)
+    {
+        if (col % 2 == 0)
+        {
+            for (int row = 0; row < m; row++)
+            {
+                cout << arr[row][col] << " ";
+            }
+        }
+        else
+        {
+            for (int row = m - 1; row >= 0; row--)
+            {
+                cout << arr[row][col] << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
