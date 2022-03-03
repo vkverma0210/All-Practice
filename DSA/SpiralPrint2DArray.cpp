7dd808653051c3943335ff697ca0f2dbabe30e7c
Vinay Kumar Verma <vkverma0210@gmail.com>
2022-03-03 23:20:15 +0530

    PiyushAndMagicalPark

--- DSA/SpiralPrint2DArray.cpp
+++ DSA/SpiralPrint2DArray.cpp
@@ -50,9 +50,9 @@ int main()
 
     int arr[n][100];
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             cin >> arr[i][j];
         }
