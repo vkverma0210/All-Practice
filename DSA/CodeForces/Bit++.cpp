7d01b7101cbd10e6aa11b00a76593cdf97e500be
Vinay Kumar Verma <vkverma0210@gmail.com>
2022-03-25 23:35:40 +0530

    Codeforces

---
+++ DSA/CodeForces/Bit++.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    while (n--)
+    {
+        string s;
+        cin >> s;
+
+        if (s[1] == '+')
+            ans++;
+
+        if (s[1] == '-')
+            ans--;
+    }
+
+    cout << ans << endl;
+}
