2acc630f05e5dc4fea7f91bb72e97a65fae4cea9
Vinay Kumar Verma <vkverma0210@gmail.com>
2022-03-28 16:41:44 +0530

    Codeforces

---
+++ DSA/CodeForces/StonesOnTheTable.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int ans = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] == s[i - 1])
+            ans++;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
