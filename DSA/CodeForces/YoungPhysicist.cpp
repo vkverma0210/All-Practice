7f8243ef1652d86fc4dc7ed8976a859d7e2e880d
vkverma0210 <vkverma0210@gmail.com>
2022-03-29 17:02:54 +0530

    Codeforces

--- DSA/CodeForces/YoungPhysicist.cpp
+++ DSA/CodeForces/YoungPhysicist.cpp
@@ -7,7 +7,28 @@ int main()
     int n;
     cin >> n;
 
-    cout << n << "\n";
+    int arr[n][3];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < 3; j++)
+            cin >> arr[i][j];
+
+    for (int i = 0; i < 3; i++)
+    {
+        int temp = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            temp += arr[j][i];
+        }
+        if (temp != 0)
+        {
+            cout << "NO \n";
+            return 0;
+        }
+    }
+
+    cout << "YES \n";
 
     return 0;
 }
