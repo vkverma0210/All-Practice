9b746b15c22a91afbfc742e87bc71f2fd5c6b320
vkverma0210 <vkverma0210@gmail.com>
2022-03-29 18:03:42 +0530

    Codeforces

---
+++ DSA/CodeForces/Football.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    int count = 1;
+
+    for (int i = 1; i <= str.length(); i++)
+    {
+        if (str[i] == str[i - 1])
+        {
+            count++;
+            
+            if (count == 7)
+            {
+                cout << "YES \n";
+                return 0;
+            }
+        }
+        else
+            count = 1;
+    }
+
+    cout << "NO \n";
+
+    return 0;
+}
