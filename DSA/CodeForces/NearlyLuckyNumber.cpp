9d379d6491b6588caa3071d9bdd904ca6e01db3e
vkverma0210 <vkverma0210@gmail.com>
2022-03-31 23:04:29 +0530

    Codeforces

---
+++ DSA/CodeForces/NearlyLuckyNumber.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    int count = 0;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        if ((str[i] == '4') || (str[i] == '7'))
+        {
+            count++;
+        }
+    }
+
+    if (count == 4 || count == 7)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
