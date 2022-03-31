a9c5ea8043308f74ba659d92467487b9b1fa6701
vkverma0210 <vkverma0210@gmail.com>
2022-03-31 16:45:51 +0530

    Wrong Subtraction

--- DSA/CodeForces/WrongSubtraction.cpp
+++ DSA/CodeForces/WrongSubtraction.cpp
@@ -7,8 +7,19 @@ int main()
     int n, k;
     cin >> n >> k;
 
+    while (k--)
+    {
+        if (n % 10 == 0)
+        {
+            n /= 10;
+        }
+        else
+        {
+            n -= 1;
+        }
+    }
 
-    
+    cout << n << endl;
 
     return 0;
 }
