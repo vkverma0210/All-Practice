98f679588d8ece3e19a51f821f019732f61105e6
vkverma0210 <vkverma0210@gmail.com>
2022-07-15 22:29:54 +0530

    15 July 2022 2

--- DSA/CODECHEF/PermutationAndMedian.java
+++ DSA/CODECHEF/PermutationAndMedian.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-import javax.lang.model.util.ElementScanner14;
-
 class PermutationAndMedian {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
