adfbf6c993560b3d2778bf86356a93b0ceda6258
vkverma0210 <vkverma0210@gmail.com>
2022-07-21 00:03:07 +0530

    21 July 2022 1

---
+++ DSA/CodingBlocks/GenerateSubsetsusingBitmasking.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void filterChars(int n, char a[])
+{
+    int j = 0;
+
+    while (n > 0)
+    {
+        int last_bit = (n & 1);
+
+        if (last_bit == 1)
+        {
+            cout << a[j];
+        }
+        j++;
+        n = n >> 1;
+    }
+
+    cout << endl;
+}
+
+void printSubsets(char a[])
+{
+    int n = strlen(a);
+
+    for (int i = 0; i < (1 << n); i++)
+    {
+        filterChars(i, a);
+    }
+
+    return;
+}
+
+int main()
+{
+    char a[100];
+    cin >> a;
+
+    printSubsets(a);
+
+    cout << endl;
+
+    return 0;
+}
