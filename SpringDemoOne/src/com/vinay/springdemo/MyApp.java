9f61eeb6ffc2c9e319930f946ae2a03d22a9cadf
vkverma0210 <vkverma0210@gmail.com>
2022-07-26 23:23:42 +0530

    26 7 2022 2

---
+++ SpringDemoOne/src/com/vinay/springdemo/MyApp.java
@@ -0,0 +1,12 @@
+package com.vinay.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// Create the object
+		Coach theCoach = new TrackCoach();
+
+		// Use the object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+}
