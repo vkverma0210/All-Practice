b180d216e747cf96ffbef11f8bc0e6be3b5c2cfa
vkverma0210 <vkverma0210@gmail.com>
2022-07-26 22:18:25 +0530

    26 July 2022 1

---
+++ Java Spring SpringBoot/SpringDemoOne/src/com/vinay/springdemo/SetterDemoApp.java
@@ -0,0 +1,25 @@
+package com.vinay.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		//Load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//reterieve bean from spring container
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+		
+		//call methods on the bean 
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		//Call our new methods to get the literalks values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+		
+		//close the contest
+		context.close();
+	}
+}
