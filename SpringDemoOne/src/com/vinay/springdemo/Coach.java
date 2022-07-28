89d3de0c54a0607fd1e99fcc13a16ea708024fb7
vkverma0210 <vkverma0210@gmail.com>
2022-07-29 00:41:47 +0530

    29 July 2022 2

--- SpringDemoOne/src/com/vinay/springdemo/Coach.java
+++
@@ -1,7 +0,0 @@
-package com.vinay.springdemo;
-
-public interface Coach {
-	public String getDailyWorkout();
-	
-	public String getDailyFortune();
-}
--- SpringDemoOne/src/com/vinay/springdemo/FortuneService.java
+++
@@ -1,5 +0,0 @@
-package com.vinay.springdemo;
-
-public interface FortuneService {
-	public String getFortune();
-}
