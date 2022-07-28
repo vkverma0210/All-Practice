93f4a5944a5e11089260ba758c94b331cfedb3da
vkverma0210 <vkverma0210@gmail.com>
2022-07-29 00:40:46 +0530

    29 July 2022 1

---
+++ Java Spring SpringBoot/SpringDemoPracticeOne/src/com/vinay/springdemo/FootballCoach.java
@@ -0,0 +1,10 @@
+package com.vinay.springdemo;
+
+public class FootballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Practice for 2 hours";
+	}
+}
---
+++ Java Spring SpringBoot/SpringDemoPracticeOne/src/com/vinay/springdemo/PracticeMain.java
@@ -0,0 +1,20 @@
+package com.vinay.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PracticeMain {
+	public static void main(String[] args) {
+		
+		//Load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myFootballCoach", Coach.class);
+		
+		//Call methodd ont he bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close the context
+		context.close();
+	}
+}
