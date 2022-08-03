7dfcb5b4c16a9fad2d7869a13b8415ac500fdce8
vkverma0210 <vkverma0210@gmail.com>
2022-08-03 19:12:15 +0530

    Commit 3 Aug 2022 2

---
+++ Java Full Stack Development/S03L020 - Exercise - Solve an Equation/src/Exercise.java
@@ -0,0 +1,8 @@
+public class Exercise {
+	public static void main(String[] args) {
+		int a = 25;
+		float b = 42.159f;
+		int  value = (int)((a * a) + (2 * a * b) + (b * b));
+		System.out.println("(a + b) ^ 2 = " + value);
+	}
+}
---
+++ Java Full Stack Development/S03L05 - Understanding Variables/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		int value1 = 1;
+		System.out.println(value1);
+	}
+}
