56dbe5e0ef81924314d605783eb1c5f2c90ee417
vkverma0210 <vkverma0210@gmail.com>
2022-08-06 22:09:48 +0530

    6 Aug 2022 2

--- DSA/LeetCode/MergeSimilarItems.java
+++ DSA/LeetCode/MergeSimilarItems.java
@@ -1,50 +1,63 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Collections;
 
 public class MergeSimilarItems {
-    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+    public static List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
         List<List<Integer>> ret = new ArrayList<>();
 
-        Arrays.sort(items1);
-        Arrays.sort(items2);
-        int size1 = items1.length;
-        int size2 = items2.length;
-        int i = 0, j = 0;
-        while (i < size1 && j < size2) {
-            if (items[i][0] == items2[j][0]) {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(items[i][1] + items2[j][1]);
-                ret.add(items1[i][0], temp);
-                i++;
-                j++;
-            } else if (items[i][0] < items2[j][0]) {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(items[i][1]);
-                ret.add(items1[i][0], temp);
-                i++;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int[] i : items1) {
+            map.put(i[0], i[1]);
+        }
+
+        for (int[] i : items2) {
+            if (map.containsKey(i[0])) {
+                map.put(i[0], map.get(i[0]) + i[1]);
             } else {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(items[j][1]);
-                ret.add(items1[j][0], temp);
-                j++;
-            }
-
-            while (i < size1) {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(items[i][1]);
-                ret.add(items1[i][0], temp);
-                i++;
-            }
-
-            while (i < size1) {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(items[j][1]);
-                ret.add(items1[j][0], temp);
-                j++;
+                map.put(i[0], i[1]);
             }
         }
 
+        for (int i : map.keySet()) {
+            ArrayList<Integer> set = new ArrayList<>();
+            set.add(i);
+            set.add(map.get(i));
+            ret.add(set);
+        }
+
+        Collections.sort(ret, (a, b) -> a.get(0) - b.get(0));
+
         return ret;
+    }
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+        int n = scr.nextInt();
+        int m = scr.nextInt();
+
+        int[][] items1 = new int[n][2];
+        int[][] items2 = new int[m][2];
+
+        for (int i = 0; i < n; i++) {
+            items1[i][0] = scr.nextInt();
+            items1[i][1] = scr.nextInt();
+        }
+
+        for (int i = 0; i < m; i++) {
+            items2[i][0] = scr.nextInt();
+            items2[i][1] = scr.nextInt();
+        }
+
+        List<List<Integer>> ans = mergeSimilarItems(items1, items2);
+
+        System.out.println(ans);
+        scr.close();
     }
 }
