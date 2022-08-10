b2f84847d016bfc78d2b589ed745407f3d706ac6
vkverma0210 <vkverma0210@gmail.com>
2022-08-10 19:05:29 +0530

    Commit 10 Aug 22 3

---
+++ DSA/LeetCode/ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class ConvertSortedArraytoBinarySearchTree {
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+
+        TreeNode head = bst(nums, 0, nums.length - 1);
+        return head;
+    }
+
+    public static TreeNode bst(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = bst(nums, start, mid - 1);
+        node.right = bst(nums, mid + 1, end);
+
+        return node;
+    }
+
+    public static void preorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        preorder(node.left);
+        System.out.print(node.val + " ");
+        preorder(node.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+        int n = scr.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scr.nextInt();
+        }
+
+        TreeNode head = sortedArrayToBST(arr);
+
+        preorder(head);
+
+        scr.close();
+    }
+}
