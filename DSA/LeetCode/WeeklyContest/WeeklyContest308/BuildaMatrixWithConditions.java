97a2e494dc6263fbcbe574bf8eebf9a43accd6c8
vkverma0210 <vkverma0210@gmail.com>
2022-08-31 00:27:23 +0530

    Commit 31 Aug 22 2

--- DSA/LeetCode/WeeklyContest/WeeklyContest308/BuildaMatrixWithConditions.java
+++ DSA/LeetCode/WeeklyContest/WeeklyContest308/BuildaMatrixWithConditions.java
@@ -8,54 +8,40 @@ import java.util.Scanner;
 
 public class BuildaMatrixWithConditions {
     public static int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
-        List<Integer> rowOrder = GenerateTopologicalSort(rowConditions, k);
-        List<Integer> colOrder = GenerateTopologicalSort(rowConditions, k);
-
-        if (rowOrder.size() < k || colOrder.size() < k)
+        List<Integer> order1 = GenerateTopologicalSort(rowConditions, k);
+        List<Integer> order2 = GenerateTopologicalSort(colConditions, k);
+        if (order1.size() < k || order2.size() < k)
             return new int[0][0];
-
-        Map<Integer, Integer> mp = new HashMap<>();
-
+        Map<Integer, Integer> m = new HashMap();
         for (int i = 0; i < k; i++)
-            mp.put(colOrder.get(i), i);
-
+            m.put(order2.get(i), i);
         int[][] ans = new int[k][k];
-
         for (int i = 0; i < k; i++)
-            ans[i][mp.get(rowOrder.get(i))] = rowOrder.get(i);
-
+            ans[i][m.get(order1.get(i))] = order1.get(i);
         return ans;
     }
 
     private static List<Integer> GenerateTopologicalSort(int[][] A, int k) {
         int[] deg = new int[k];
-        List<Integer> order = new ArrayList<>();
-        List<List<Integer>> graph = new ArrayList<>();
-
+        List<Integer> order = new ArrayList();
+        List<List<Integer>> graph = new ArrayList();
         for (int i = 0; i < k; i++)
-            graph.add(new ArrayList<>());
-
-        Queue<Integer> q = new LinkedList<>();
-
+            graph.add(new ArrayList());
+        Queue<Integer> q = new LinkedList();
         for (int[] c : A) {
             graph.get(c[0] - 1).add(c[1] - 1);
             deg[c[1] - 1]++;
         }
-
         for (int i = 0; i < k; i++)
             if (deg[i] == 0)
                 q.add(i);
-
         while (!q.isEmpty()) {
             int x = q.poll();
-
             order.add(x + 1);
-
             for (int y : graph.get(x))
                 if (--deg[y] == 0)
                     q.add(y);
         }
-
         return order;
     }
 
