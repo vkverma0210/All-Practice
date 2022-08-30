0413c26d164ccd752316ae2273446a0da61cba33
vkverma0210 <vkverma0210@gmail.com>
2022-08-30 20:15:08 +0530

    Commit 30 Aug 22 1

---
+++ DSA/LeetCode/DailyProblems_Aug22/RotateImage.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+        int n = scr.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scr.nextInt();
+            }
+        }
+
+        rotate(arr);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+        scr.close();
+    }
+}
