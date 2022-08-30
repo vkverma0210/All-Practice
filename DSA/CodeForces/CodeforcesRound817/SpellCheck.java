06a0e29b715b41778dc7c2ad9715180aa72c976e
vkverma0210 <vkverma0210@gmail.com>
2022-08-30 22:55:10 +0530

    Commit 30 Aug 22 5

--- DSA/CodeForces/CodeforcesRound817/SpellCheck.java
+++ DSA/CodeForces/CodeforcesRound817/SpellCheck.java
@@ -3,31 +3,6 @@ import java.util.Set;
 import java.util.Scanner;
 
 public class SpellCheck {
-    public static void solve(String str) {
-        if (str.length() > 5) {
-            System.out.println("NO");
-            return;
-        }
-        Set<Character> s = new HashSet<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            s.add(str.charAt(i));
-        }
-
-        char[] temp = { 'T', 'i', 'm', 'u', 'r' };
-
-        for (int i = 0; i < 5; i++) {
-            if (s.contains(temp[i]))
-                s.remove(temp[i]);
-        }
-
-        if (s.size() == 0) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
-    }
-
     public static void main(String[] args) {
 
         Scanner scr = new Scanner(System.in);
@@ -36,10 +11,13 @@ public class SpellCheck {
 
         while (t != 0) {
             int n = scr.nextInt();
-
             String str = scr.next();
 
-            solve(str);
+            if (str.contains("T") && str.contains("i") && str.contains("m") && str.contains("u") && str.contains("r")
+                    && str.length() == 5)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
 
             t--;
         }
