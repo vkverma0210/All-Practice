660e16cec4d470f2fcb9cda30d41c026fb77566a
vkverma0210 <vkverma0210@gmail.com>
2022-09-07 00:17:29 +0530

    Commit 7 Sept 22 2

--- DSA/LeetCode/WeeklyContest/BiweeklyContest86/MaximumRowsCoveredbyColumns.java
+++ DSA/LeetCode/WeeklyContest/BiweeklyContest86/MaximumRowsCoveredbyColumns.java
@@ -1,39 +1,42 @@
 import java.util.Scanner;
 
 public class MaximumRowsCoveredbyColumns {
-    public static int maximumRows(int[][] matrix, int numSelect) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[] visited = new int[n];
+    public int maximumRows(int[][] mat, int cols) {
 
-        return maximumRowsHelper(0, m, n, numSelect, matrix, visited);
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] s = new int[n];
+
+        return f(0, m, n, cols, mat, s);
     }
 
-    public static int maximumRowsHelper(int index, int m, int n, int numSelect, int[][] matrix, int[] visited) {
-        if (numSelect == 0) {
+    public int f(int ind, int m, int n, int cols, int[][] mat, int[] s) {
+
+        if (cols == 0) {
             int count = 0;
-
             for (int i = 0; i < m; i++) {
+                // supposing that this row is valid
                 boolean selected = true;
-
                 for (int j = 0; j < n; j++) {
-                    if (matrix[i][j] == 1 && visited[j] != 1)
+                    // if any cell of this row violates the given two conditions, then the row is
+                    // discarded
+                    if (mat[i][j] == 1 && s[j] != 1)
                         selected = false;
                 }
-
                 if (selected)
-                    count++;
+                    count += 1;
             }
 
             return count;
         }
 
         int ans = -1;
+        for (int i = ind; i < n; i++) {
 
-        for (int i = index; i < n; i++) {
-            visited[i] = 1;
-            ans = Math.max(ans, maximumRowsHelper(index + 1, m, n, numSelect - 1, matrix, visited));
-            visited[i] = 0;
+            s[i] = 1; // do
+            ans = Math.max(ans, f(i + 1, m, n, cols - 1, mat, s));
+            s[i] = 0; // undo / backtrack
         }
 
         return ans;
