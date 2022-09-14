fdbf59050a1fbd202ef15f687862b9ff1ac00812
vkverma0210 <vkverma0210@gmail.com>
2022-09-14 23:27:59 +0530

    Commit 14 Sept 22 5

--- Java Full Stack Udemy/HelloWorld/src/main/java/org/studyeasy/Hello.java
+++ Java Full Stack Udemy/HelloWorld/src/main/java/org/studyeasy/Hello.java
@@ -14,27 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public Hello() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public Hello() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().println("<h1> Hello World</h1>");
 	}
-
 }
