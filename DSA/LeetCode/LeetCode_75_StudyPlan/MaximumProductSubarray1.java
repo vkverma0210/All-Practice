46492069fb795a578aef00701adf19e0979265aa
vkverma0210 <vkverma0210@gmail.com>
2022-09-28 23:04:39 +0530

    Commit 28 Sept 22 3

--- DSA/LeetCode/LeetCode_75_StudyPlan/MaximumProductSubarray1.java
+++ DSA/LeetCode/LeetCode_75_StudyPlan/MaximumProductSubarray1.java
@@ -2,18 +2,15 @@ import java.util.Scanner;
 
 public class MaximumProductSubarray1 {
     public static int maxProduct(int[] nums) {
-        int currentProduct = nums[0];
+        int currProduct = nums[0];
         int maxProductSoFar = nums[0];
         int minEndingHere = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
-            int temp = currentProduct;
-
-            currentProduct = Math.max(nums[i], Math.max(currentProduct * nums[i], nums[i] * minEndingHere));
-
+            int temp = currProduct;
+            currProduct = Math.max(nums[i], Math.max(currProduct * nums[i], nums[i] * minEndingHere));
             minEndingHere = Math.min(nums[i], Math.min(temp * nums[i], nums[i] * minEndingHere));
-
-            maxProductSoFar = Math.max(currentProduct, maxProductSoFar);
+            maxProductSoFar = Math.max(currProduct, maxProductSoFar);
         }
 
         return maxProductSoFar;
@@ -23,17 +20,13 @@ public class MaximumProductSubarray1 {
         Scanner scr = new Scanner(System.in);
 
         int n = scr.nextInt();
-
         int[] nums = new int[n];
 
         for (int i = 0; i < n; i++) {
             nums[i] = scr.nextInt();
         }
 
-        int ans = maxProduct(nums);
-
-        System.out.println(ans);
-
+        System.out.println(maxProduct(nums));
         scr.close();
     }
 }
