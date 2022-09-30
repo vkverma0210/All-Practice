25a0b4c051779966df990e28e7349248c2604bc1
vkverma0210 <vkverma0210@gmail.com>
2022-09-30 19:58:41 +0530

    Commit 30 Sept 22 4

--- DSA/LeetCode/LeetCode_75_StudyPlan/SymmetricTree.java
+++ DSA/LeetCode/LeetCode_75_StudyPlan/SymmetricTree.java
@@ -1,3 +1,95 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
 public class SymmetricTree {
-    
+    // 1. Recursive
+    public static boolean isSymmetric1(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isMirror(root.left, root.right);
+    }
+
+    private static boolean isMirror(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return (p.val == q.val) && isMirror(p.left, q.right) && isMirror(p.right, q.left);
+    }
+
+    // 2. Iterative
+    public static boolean isSymmetric2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+
+        while (q.size() > 1) {
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null ^ right == null) {
+                return false;
+            }
+
+            if (left.val != right.val) {
+                return false;
+            }
+
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
+        }
+
+        return true;
+    }
+
+    public static void preorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.val + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree1 = new TreeNode(1);
+        tree1.left = new TreeNode(0);
+        tree1.right = new TreeNode(1);
+        tree1.left.left = new TreeNode(0);
+        tree1.left.right = new TreeNode(0);
+        tree1.right.left = new TreeNode(0);
+        tree1.right.right = new TreeNode(1);
+
+        System.out.println("Tree1");
+        preorder(tree1);
+        System.out.println();
+
+        System.out.println(isSymmetric1(tree1));
+        System.out.println(isSymmetric2(tree1));
+    }
 }
