658c9e7d8dbc35df6c5c057c04fd1f0aa040030e
vkverma0210 <vkverma0210@gmail.com>
2022-10-01 12:04:40 +0530

    Commit 1 Oct 22 1

--- Hadoop/MapReduce/mostcommonword_reducer.py
+++ Hadoop/MapReduce/mostcommonword_reducer.py
@@ -2,23 +2,26 @@
 
 import sys
 
-dict1={}				# Define a dictionary that will contain key value pair. Key will be each word from mapper and value will be 1 as assigned by mapper for each word					
-firsttime = "true"				
+dict1 = {}				# Define a dictionary that will contain key value pair. Key will be each word from mapper and value will be 1 as assigned by mapper for each word
+firsttime = "true"
 
 for line in sys.stdin:
- word,count=line.split(',')		# Split the word and 1 assigned in the mapper into variable word and count
+    # Split the word and 1 assigned in the mapper into variable word and count
+    word, count = line.split(',')
 
- if(firsttime=="true"):				
-  dict1[word]=int(count)		# When coming for first time, dictionary will be empty. Hence just move the (word, count) in dictionary
-  firsttime="false"
+    if (firsttime == "true"):
+        # When coming for first time, dictionary will be empty. Hence just move the (word, count) in dictionary
+        dict1[word] = int(count)
+        firsttime = "false"
+
+    wordexist = [key for key in dict1]  # Check if word exists in dictionary
+    if (word in wordexist):
+        # If the word exist, just add the count
+        dict1[word] = dict1[word]+int(count)
+    else:
+        # If the word does not exist, just move the (word, count) in dictio
+        dict1[word] = int(count)
 
 
- wordexist=[key for key in dict1]	# Check if word exists in dictionary
- if(word in wordexist):
-   dict1[word]=dict1[word]+int(count)	# If the word exist, just add the count 
- else:
-   dict1[word]=int(count)		# If the word does not exist, just move the (word, count) in dictionary
-
-
-maximum=max(dict1,key=dict1.get)
-print(maximum,dict1[maximum])
+maximum = max(dict1, key=dict1.get)
+print(maximum, dict1[maximum])
