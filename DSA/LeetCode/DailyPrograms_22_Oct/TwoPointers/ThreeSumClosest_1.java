99ec5615aba37978ec240f17fa43863e24ca67c5
vkverma0210 <vkverma0210@gmail.com>
2022-10-08 23:58:10 +0530

    Commit 8 Oct 22 1

---
+++ DSA/LeetCode/DailyPrograms_22_Oct/TwoPointers/ThreeSumClosest_1.java
@@ -0,0 +1,17 @@
+
+public class ThreeSumClosest_1 {
+public static int threeSumClosest(int[]nums,int target){
+    Arrays.sort(nums);
+    int diff = Integer.MAX_VALUE;
+
+    for(int i = 0; i < nums.length - ; i++){
+        int left = i + 1;
+        int right = nums.length - 1;
+    }
+    
+}
+
+    public static void main(String[] args) {
+
+    }
+}
