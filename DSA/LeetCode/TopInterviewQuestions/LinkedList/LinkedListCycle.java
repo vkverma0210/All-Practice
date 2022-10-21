10bcbdcc07a45577de79d75e84e6a875772ecd3a
vkverma0210 <vkverma0210@gmail.com>
2022-10-21 23:32:47 +0530

    Commit 21 Oct 22 2

---
+++ DSA/LeetCode/TopInterviewQuestions/LinkedList/LinkedListCycle.java
@@ -0,0 +1,50 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class LinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(3);
+        head.next.next = new ListNode(4);
+        head.next.next.next = new ListNode(7);
+        head.next.next.next.next = new ListNode(1);
+        head.next.next.next.next.next = new ListNode(2);
+        head.next.next.next.next.next.next = new ListNode(6);
+
+        System.out.println("Given Linked list");
+        printList(head);
+        System.out.println(hasCycle(head));
+        printList(head);
+    }
+}
