799bedaebff7069d643070805056cfaf61f0178a
vkverma0210 <vkverma0210@gmail.com>
2022-10-21 23:24:19 +0530

    Commit 21 Oct 22 1

---
+++ DSA/LeetCode/DailyPrograms_22_Oct/Array/ContainsDuplicateII.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ContainsDuplicateII {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int n = scr.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scr.nextInt();
+        }
+
+        int k = scr.nextInt();
+        System.out.println(containsNearbyDuplicate(nums, k));
+
+        scr.close();
+    }
+}
