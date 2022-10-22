79b5032814f523811173a13b43a43dd5d47dbf2f
vkverma0210 <vkverma0210@gmail.com>
2022-10-22 22:31:42 +0530

    Commit 22 Oct 22 2

---
+++ DSA/LeetCode/TopInterviewQuestions/Arrays/MajorityElement.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int majorityEle = nums[0], count = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                count++;
+                majorityEle = nums[i];
+            } else if (majorityEle == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return majorityEle;
+    }
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int n = scr.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scr.nextInt();
+        }
+
+        System.out.println(majorityElement(nums));
+
+        scr.close();
+    }
+}
