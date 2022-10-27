d1c6f88454c5c685c17209322bee63e92443a989
vkverma0210 <vkverma0210@gmail.com>
2022-10-27 23:56:16 +0530

    Commit 27 Oct 22 7

---
+++ DSA/LeetCode/OtherTopicWiseQuestions/LinkedList/AddTwoNumbers.java
@@ -0,0 +1,66 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class AddTwoNumbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry == 1) {
+            int sum = 0;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            curr.next = node;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+
+    private static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        System.out.println("Given Linked list 1");
+        printList(l1);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        System.out.println("Given Linked list 2");
+        printList(l2);
+
+        ListNode ans = addTwoNumbers(l1, l2);
+        System.out.println("Result Linked list");
+        printList(ans);
+    }
+}
