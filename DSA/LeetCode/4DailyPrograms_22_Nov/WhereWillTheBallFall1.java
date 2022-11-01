b6ffdeb53483be834fa98cd405765c453d894351
vkverma0210 <vkverma0210@gmail.com>
2022-11-01 23:35:09 +0530

    Commit 1 Nov 22 1

--- DSA/LeetCode/4DailyPrograms_22_Nov/WhereWillTheBallFall1.java
+++ DSA/LeetCode/4DailyPrograms_22_Nov/WhereWillTheBallFall1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WhereWillTheBallFall {
+public class WhereWillTheBallFall1 {
     public static int[] findBall(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
