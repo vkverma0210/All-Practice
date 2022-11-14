422a3495f35650c00854595416c3ef9e7f656f49
vkverma0210 <vkverma0210@gmail.com>
2022-11-14 19:31:35 +0530

    Commit 14 Nov 22 1

---
+++ DSA/LeetCode/4DailyPrograms_22_Nov/Graph/MostStonesRemovedWithSameRowOrColumn1.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class MostStonesRemovedWithSameRowOrColumn1 {
+    public static int removeStones(int[][] stones) {
+        Set<int[]> visited = new HashSet<>();
+
+        int numOfIslands = 0;
+
+        for (int[] s1 : stones) {
+            if (!visited.contains(s1)) {
+                dfs(s1, visited, stones);
+                numOfIslands++;
+            }
+        }
+
+        return stones.length - numOfIslands;
+    }
+
+    private static void dfs(int[] s1, Set<int[]> visited, int[][] stones) {
+        visited.add(s1);
+
+        for (int[] s2 : stones) {
+            if (!visited.contains(s2)) {
+                if (s1[0] == s2[0] || s1[1] == s2[1]) {
+                    dfs(s2, visited, stones);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+        int n = scr.nextInt();
+        int[][] stones = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            stones[i][0] = scr.nextInt();
+            stones[i][1] = scr.nextInt();
+        }
+
+        System.out.println(removeStones(stones));
+
+        scr.close();
+    }
+}
