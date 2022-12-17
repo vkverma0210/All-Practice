381573e60b64fb78fff4aef12435389014ef4454
vkverma0210 <vkverma0210@gmail.com>
2022-12-17 18:32:41 +0530

    Commit 17 Dec 22 2

--- DSA/LeetCode/5DailyPrograms_22_Dec/DP/NumberOfLongestIncreasingSubsequence.java
+++ DSA/LeetCode/5DailyPrograms_22_Dec/DP/NumberOfLongestIncreasingSubsequence.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class NumberOfLongestIncreasingSubsequence {
-    public int findNumberOfLIS(int[] nums) {
+    public static int findNumberOfLIS(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
@@ -11,18 +12,36 @@ public class NumberOfLongestIncreasingSubsequence {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
                     dp[i] = dp[j] + 1;
                     count[i] = count[j];
-                } else if (arr[j] < arr[i] && dp[j] + 1 == dp[i]) {
+                } else if (nums[j] < nums[i] && dp[j] + 1 == dp[i]) {
                     count[i] = count[i] + count[j];
                 }
             }
+
+            maxi = Math.max(maxi, dp[i]);
         }
 
+        int nos = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == maxi) {
+                nos += count[i];
+            }
+        }
+
+        return nos;
     }
 
     public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int n = scr.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scr.nextInt();
+        }
 
+        System.out.println(findNumberOfLIS(nums));
+        scr.close();
     }
 }
